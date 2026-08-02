a78eb258a9ff5d50cf98b912bb5fbc1cac2f9e2c
dullamanojreddy <dullamanojreddy@gmail.com>
2026-08-02 13:31:54 +0530

    Fix seed.js: load dotenv with absolute path so MONGO_URI is available

--- server/src/seed/seed.js
+++ server/src/seed/seed.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: require("path").resolve(__dirname, "../../.env") });
 const mongoose = require("mongoose");
 const { connectDB, getMongoUri } = require("../config/db");
 const Profile = require("../models/Profile");
